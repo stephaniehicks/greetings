4c3ba5f3b8e1808f4088df5cc001febd54cf54a3
Stephanie Hicks <stephaniechicks@gmail.com>
2024-11-26 14:34:55 -0500

    initial commit

---
+++ R/hello.R
@@ -0,0 +1,24 @@
+#' Print a Greeting
+#'
+#' Print a greeting for a custom name
+#'
+#' @details This function make a plot with a greeting to the
+#'          name passed as an argument to the function
+#'
+#' @param name character, name of person to whom greeting should be directed
+#'
+#' @return nothing useful is returned.
+#'
+#' @import ggplot2
+#' @export
+#'
+#' @examples
+#' hello("Chris")
+#'
+hello <- function(name) {
+        message <- paste0("Hello, ", name, "!")
+        ggplot() +
+                geom_text(aes(0, 0), label = message, size = 4) +
+                theme_minimal()
+
+}
